ab514710662e61fb5f5c886d6f1f4976800ab884
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2021-01-16 15:31:11 +0530

    first commit

---
+++ AddTwoNumbers_E.cpp
@@ -0,0 +1,16 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    int T ; cin >>T;
+    for(int i=0; i<T; i++){
+        int a, b;
+        cin >> a>>b;
+        cout << a+b << endl;
+    }
+
+	return 0;
+}
